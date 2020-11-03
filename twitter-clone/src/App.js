42187678a84b434e2e23b06936dd91d03ec594ff
keenan560 <keenan560@gmail.com>
2020-11-02 18:06:55 -0600

    built sidebar

---
+++ twitter-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="app">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Feed */}
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
